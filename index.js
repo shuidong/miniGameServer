d82cd1637e5a327113a8ecb77049c5f590b3623a
shuidong <lsd751@qq.com>
2017-03-29 21:58:00 +0800

    添加了 开发日志 changelog

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ app.get('/echo', function(req, res, next){
 	//注意这种提取方式完全是bodyParser的功劳，否则要自己对res监听data与end事件才行
 	//对于http://127.0.0.1:3000/echo?a=99&b=nihao
 	//服务器这里将会打印 { a: '99', b: 'nihao' }
-	console.dir(req.query);
+	//console.dir(req.query);
 
 	//2.把传过来的数据再回传过去
+	res.contentType('json');
 	res.send(JSON.stringify(req.query));
 	//3.关闭这个流
 	res.end();
